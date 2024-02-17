40142711a9bb446030a4329a3a9862475cc5ba4d
Atis <info@amstorm.com>
2024-02-17 06:44:57 +0200

    Typesafety project

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,20 +9,17 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
     // add social providers
-    // externalProviders: {
-    /**
-     * first, create your secrets using `amplify sandbox secret`
-     * then, import `secret` from `@aws-amplify/backend`
-     * @see https://docs.amplify.aws/gen2/deploy-and-host/sandbox-environments/features/#setting-secrets
-     */
-    // loginWithAmazon: {
-    //   clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
-    //   clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
-    // }
-    // configure callback and logout URLs
-    // callbackUrls: ['http://localhost:3000'],
-    // logoutUrls: ['http://localhost:3000'],
-    // },
+    externalProviders: {
+      /**
+       * first, create your secrets using `amplify sandbox secret`
+       * then, import `secret` from `@aws-amplify/backend`
+       * @see https://docs.amplify.aws/gen2/deploy-and-host/sandbox-environments/features/#setting-secrets
+       */
+      // loginWithAmazon: {
+      //   clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+      //   clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
+      // }
+    },
   },
   /**
    * enable multifactor authentication
